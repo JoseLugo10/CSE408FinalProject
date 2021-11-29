f8a137cb82ced4995f8970b50236e7cb8fc99ca2
akaAngel <56285402+akaAngel@users.noreply.github.com>
2021-11-29 16:41:47 -0700

    I have added the wordsWithStrength table. The table is sorted by descending value of strength. In orger to do this i had to convert the cells to matrix because without so i couldnt scale with right wing by -1 and add it to the table because the leftwing stength column was of non-cell type so i had to convert that aswell.

--- main.m
+++ main.m
@@ -26,3 +26,21 @@ for file = leftFiles'
     featVec = createBOW(fullfile(file.folder,file.name), rightVoc(1,:));
     rightFeatSet = [rightFeatSet,featVec'];
 end
+
+% wordsWithStrength is a list of the Sentiment Value of each word
+temp = cell2mat(rightVoc(2,:).');
+temp(:,1) = temp(:,1)*-1;
+
+Rtable = table;
+Ltable = table;
+Rtable.words = rightVoc(1,:).';
+Rtable.strength = temp;
+
+temp = cell2mat(leftVoc(2,:).');
+temp(:,1) = temp(:,1)*1;
+
+Ltable.words = leftVoc(1,:).';
+Ltable.strength = temp;
+
+wordsWithStrength = [Ltable;Rtable];
+wordsWithStrength = sortrows(wordsWithStrength,'strength','descend');
