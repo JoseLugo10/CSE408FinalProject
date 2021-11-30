c3e1d6ca11914158a3ef79ab729c6ff8c6fcea2d
akaAngel <56285402+akaAngel@users.noreply.github.com>
2021-11-30 16:22:18 -0700

    - The BoW stuff has been removed - I have put together the two vocabularies into one list known as wordsWithStrength. and it is sorted by the values so +1 at top and -1 at bottom
    **Curent issue. when running you can see that abortion is on the list twice. at: 
    67 with a value of 0.4444 and
    138 with a value of -1

--- main.m
+++ main.m
@@ -11,36 +11,17 @@ rightVoc = buildVoc(negFolder);
 leftFiles = dir(fullfile(posFolder,'*.txt'));
 rightFiles = dir(fullfile(negFolder,'*.txt'));
 
-%leftLabelSet = [];
-leftFeatSet = [];
-for file = leftFiles'
-    %leftLabelSet =  %TODO
-    featVec = createBOW(fullfile(file.folder,file.name), leftVoc(1,:));
-    leftFeatSet = [leftFeatSet,featVec'];
-end
-
-%rightLabelSet = [];
-rightFeatSet = [];
-for file = leftFiles'
-    %rightLabelSet =  %TODO
-    featVec = createBOW(fullfile(file.folder,file.name), rightVoc(1,:));
-    rightFeatSet = [rightFeatSet,featVec'];
-end
-
 % wordsWithStrength is a list of the Sentiment Value of each word
-temp = cell2mat(rightVoc(2,:).');
-temp(:,1) = temp(:,1)*-1;
 
-Rtable = table;
-Ltable = table;
-Rtable.words = rightVoc(1,:).';
-Rtable.strength = temp;
+Rtable(:,1) = (rightVoc(1,:).');
+Rtable(:,2) = rightVoc(2,:).';
 
-temp = cell2mat(leftVoc(2,:).');
-temp(:,1) = temp(:,1)*1;
-
-Ltable.words = leftVoc(1,:).';
-Ltable.strength = temp;
+Ltable(:,1) = leftVoc(1,:).';
+Ltable(:,2) = leftVoc(2,:).';
 
 wordsWithStrength = [Ltable;Rtable];
-wordsWithStrength = sortrows(wordsWithStrength,'strength','descend');
+wordsWithStrength(:,2) = sortrows(wordsWithStrength(:,2),'descend');
+
+% Debug----------------------------------------
+testing = sentiment_Analysis(wordsWithStrength);
+% Debug----------------------------------------
